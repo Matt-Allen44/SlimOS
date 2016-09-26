4560473776ed6c021c8f82b222a7adc35ed01257
= <whosdaz@gmail.com>
2016-09-26 22:57:07 +0930

    Various UI changes, added printNoStyle function, misc dev

--- kernel.c
+++ kernel.c
@@ -4,11 +4,14 @@
 char* interruptsEnabled();
 int clearScreen();
 int print();
+int printNoStyle();
 int println();
-int drawCursor(int offset);
+int drawCursor(int cell);
+int drawHeader();
 int wait();
 char* getKeyCharFromScanCode(unsigned int scancode);
 static __inline unsigned char inb(unsigned short int port);
+static inline void outb(unsigned char value, unsigned short int port);
 /* End of Declerations *
 
 /* Start Of Colors */
@@ -25,33 +28,97 @@ const int GRAY = 0x78;
 
 
 char *VideoMemPointer = (char*)0xb8000; //Pointer for the beggining of the video memory
-unsigned int i = 0;
-unsigned int p = 0; //Pointer location
+unsigned int i = 2;
+unsigned int p = 160; //Pointer location
 
 void smain(void){
   clearScreen();
-  print("SlimOS > ", MAGENTA);
+
+
+  println("   _____ _      _____ __  __  ____   _____", GRAY);
+  println(" / ____| |    |_   _|  \\/  |/ __ \\ / ____|", GRAY);
+  println("| (___ | |      | | | \\  / | |  | | (_", GRAY);
+  println(" \\___ \\| |      | | | |\\/| | |  | |\\___ \\", GRAY);
+  println(" ____) | |____ _| |_| |  | | |__| |____) |", GRAY);
+  println("|_____/|______|_____|_|  |_|\\____/|_____/", GRAY);
+  println("", GRAY);
+
+  print("[OK] ", BLUE);
   println("Cleared Screen", BLACK);
 
-  print("SlimOS > ", MAGENTA);
-  print("IRQ Supported: ", BLACK);
-  println(interruptsEnabled(), BLACK);
+  if(interruptsEnabled() == "TRUE"){
+    print("[OK] ", BLUE);
+  } else {
+    print("[FAIL] ", RED);
+  }
+
+  println("IRQ Support Initialized ", BLACK);
+
+  print("[OK] ", BLUE);
+  println("PS2 Keyboard/Mouse Initialized", BLACK);
+
+  print("[OK] ", BLUE);
+  print("Loaded Color Palette - ", BLACK);
+
+  print("BLACK ", BLACK);
+  print("BLUE ", BLUE);
+  print("GREEN ", GREEN);
+  print("CYAN ", CYAN);
+  print("RED ", RED);
+  print("MAGENTA ", MAGENTA);
+  print("BROWN ", BROWN);
+  println("GRAY ", GRAY);
 
   print("SlimOS > ", MAGENTA);
 
   unsigned char lastChar;
   unsigned char currChar;
+
+  unsigned int l = 0;
+  unsigned int lr = 1;
+
   while(1==1){
     currChar = inb(0x60);
     if(currChar != lastChar){
       lastChar = currChar;
       print(getKeyCharFromScanCode(currChar), BLACK);
 
+      //F1
       if(currChar == 0x3B){
         clearScreen();
         print("SlimOS > ", MAGENTA);
       }
+
+      //F2
+      if(currChar == 0x3C){
+      }
+
     }
+
+    /*
+    while(lr < 80){
+      if(l == 0){
+        VideoMemPointer[lr*2+1] = 0x00;;
+      } else if(l == 1){
+        VideoMemPointer[lr*2+1] = 0x10;;
+      } else if(l == 2){
+        VideoMemPointer[lr*2+1] = 0x20;;
+      } else if(l == 3){
+        VideoMemPointer[lr*2+1] = 0x34;;
+      } else if(l == 4){
+        VideoMemPointer[lr*2+1] = 0x40;;
+      } else if(l == 5){
+        VideoMemPointer[lr*2+1] = 0x50;;
+      } else if(l == 6){
+        VideoMemPointer[lr*2+1] = 0x60;;
+        l=-1;
+      }
+      lr = lr+1;
+      l++;
+    }
+    lr = 0;
+    */
+
   }
 
   return;
@@ -71,7 +138,7 @@ char* interruptsEnabled(){
 }
 
 char *getKeyCharFromScanCode(unsigned int scancode){
-  drawCursor(1);
+  drawCursor(i+1);
   if(scancode == 0x02){
     return "1";
   } else if (scancode == 0x03){
@@ -167,7 +234,7 @@ char *getKeyCharFromScanCode(unsigned int scancode){
     return "C";
   } else if (scancode == 0x2F){
     return "V";
-  } else if(scancode == 0x30){
+  } else if(scancode == 0x34){
     return "B";
   } else if(scancode == 0x31){
     return "N";
@@ -183,7 +250,7 @@ char *getKeyCharFromScanCode(unsigned int scancode){
     return "RSHIFT";
   } else if (scancode == 0x37){
     return "*";
-  } else if (scancode == 0x38){
+  } else if (scancode == 0x34){
     return "LALT";
   } else if (scancode == 0x39){
     return " ";
@@ -203,7 +270,7 @@ char *getKeyCharFromScanCode(unsigned int scancode){
     return "F6";
   } else if(scancode == 0x41){
     return "F7";
-  } else if(scancode == 0x42){
+  } else if(scancode == 0x34){
     return "F8";
   } else if (scancode == 0x43){
     return "F9";
@@ -217,7 +284,6 @@ char *getKeyCharFromScanCode(unsigned int scancode){
     //UP ARROW
   } else if (scancode == 0x4D){
     //RIGHT ARROW
-    drawCursor(3);
   } else if (scancode == 0x50){
     //DOWN ARROW
   } else if (scancode == 0X48){
@@ -226,22 +292,53 @@ char *getKeyCharFromScanCode(unsigned int scancode){
   return "";
 }
 
-int clearScreen(){
-  unsigned int j = 0;
+int drawHeader(){
+  int j2 = 0;
+  while(j2 < 80*2){
+      VideoMemPointer[j2+1] = 0x87;
+      j2 = j2 + 2;
+  }
 
+  printNoStyle(" SlimOS");
+
+  i = 160-74;
+  printNoStyle("https://github.com/matt-allen44/quik");
+}
+
+int clearScreen(){
+  p = 0;
+  i = 0;
+  unsigned int j = 0;
   /* loop through all of video memory and clear it */
   while(j < 80*25*2){
     VideoMemPointer[j] = ' ';
     VideoMemPointer[j+1] = 0x72;
+
     j = j + 2; //increment to next section of screen (as 2 bytes per section)
   }
+
+  drawHeader();
+
   j = 0;
-  i = 0;
+  i = 160;
 }
 
-int drawCursor(int offset){
-  p = p+offset;
-  VideoMemPointer[p] = 0x00;
+int drawCursor(int cell){
+  VideoMemPointer[cell] = 0x27;
+}
+
+int printNoStyle(char *str){
+  unsigned int j = 0;
+
+  while(str[j] != '\0'){
+    /* get character from string and copy to video memory */
+    VideoMemPointer[i] = str[j];
+    /* set characters colour to green */
+    j = j+1;
+    i = i+2;
+
+    p = i;
+  }
 }
 
 int print(char *str, unsigned int col){
@@ -254,8 +351,9 @@ int print(char *str, unsigned int col){
     VideoMemPointer[i+1] = col;
     j = j+1;
     i = i+2;
+
+    p = i;
   }
-  //p=i;
 }
 
 static __inline unsigned char inb (unsigned short int port)
@@ -266,6 +364,11 @@ static __inline unsigned char inb (unsigned short int port)
   return _v;
 }
 
+static inline void outb(unsigned char value, unsigned short int port)
+{
+        asm volatile ("outb %0, %1"::"a"(value), "Nd"(port));
+}
+
 /*  COLORS TABLE
     0 - Black, 1 - Blue, 2 - Green, 3 - Cyan, 4 - Red, 5 - Magenta, 6 - Brown
     7 - Light Grey, 8 - Dark Grey, 9 - Light Blue, 10/a - Light Green, 11/b - Light Cyan
@@ -274,8 +377,9 @@ static __inline unsigned char inb (unsigned short int port)
 int println(char *str, unsigned int col){
   print(str, col);
 
+  VideoMemPointer[i+3] = 0x72; //Clear cursor at the end of a line
   int bytesInALine = 160;
-  i = bytesInALine * (i/bytesInALine) + bytesInALine;
+  i = bytesInALine * (i/bytesInALine) + bytesInALine + 2;
 }
 
 int wait(){
